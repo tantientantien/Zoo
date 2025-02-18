47f45feaa534b9a82584c240cadf51d2910227f7
Nguyen Tan Tien <chaotien2003@gmail.com>
2025-02-18 16:28:41 +0700

    LINQ Usage

---
+++ NguyenTanTien/Zoo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenTanTien
+{
+    public class Zoo
+    {
+        public List<Animal> animals { get; private set; } = new List<Animal>();
+
+        public void addAnimal(Animal animal)
+        {
+            if (animal == null)
+                throw new ArgumentNullException(nameof(animal), "Animal cannot be null");
+
+            animals.Add(animal);
+        }
+
+
+        public List<Animal> getAnimals(int? age = null, string species = null)
+        {
+            return animals.Where(a =>
+                (!age.HasValue || a.getAge() >= age.Value) &&
+                (string.IsNullOrEmpty(species) || a.getSpecies().Equals(species, StringComparison.OrdinalIgnoreCase))
+            ).ToList();
+        }
+    }
+}
